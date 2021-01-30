9257532a6ef04432f98ccc34df071c28245a9778
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2021-01-30 12:44:31 +0000

    up to date

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform target;
+    public float lag = 0.3f;
+
+    Vector3 newPosition;
+
+    void FixedUpdate()
+    {
+        // position
+        newPosition = target.position - target.forward * 90f + target.up * 25f;
+        transform.position = Vector3.Slerp(transform.position, newPosition, lag);
+        // rotation
+        transform.LookAt(target.position + target.forward * 2000f);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float thrustMultiplier = 2000f;
+    public float torqueMultiplier = 2000f;
+
+    Rigidbody rb;
+    Vector3 torque;
+    float thrust;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        torque = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
+    }
+
+    void FixedUpdate()
+    {
+        // pitch and roll
+        rb.AddRelativeTorque(torque * torqueMultiplier);
+
+        // thrust
+        thrust = thrustMultiplier * Time.fixedDeltaTime;
+        thrust -= transform.forward.y * 3f;
+        rb.AddRelativeForce(new Vector3(0, 0, thrust));
+    }
+}
